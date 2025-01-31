05814b8ebeb393680a8f30465894ec594e5b894f
Lynx9456 <805324374@qq.com>
2025-01-31 22:37:30 +0800

    first commit 1-5

---
+++ src/main/java/com/example/selfspring/SelfspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.selfspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfspringApplication.class, args);
+    }
+
+}
